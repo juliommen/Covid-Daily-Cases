833e87092e83b8862f7dc8adf9919cdc43b2480a
juliommen <juliommen@gmail.com>
2022-05-28 00:41:34 -0400

    midleware error

--- src/server.js
+++ src/server.js
@@ -16,6 +16,13 @@ app.use(
     }),
 );
 
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        errors: err.errors,
+    });
+});
+
 const dbName = "kaggle";
 const collectionName = "covid-daily-cases";
 
